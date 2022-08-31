cdc69357f6e0d3bae1059dd6730f33e0841a55de
Filipe Lauren <filipelauren@gmail.com>
2022-08-31 11:31:27 -0300

    primeiro commit

---
+++ lib/main.dart
@@ -0,0 +1,11 @@
+import 'package:busca_gifs/ui/gif_page.dart';
+import 'package:flutter/material.dart';
+import 'ui/home_page.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: HomePage(),
+    debugShowCheckedModeBanner: false,
+    theme: ThemeData(hintColor: Colors.white),
+  ));
+}
